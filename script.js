4fd5b0c01d79ea9cf182ce7723bf1836298a7a42
Augusto Sergio Roque Miranda <57109047+AugustoMiranda@users.noreply.github.com>
2022-03-04 13:10:52 -0300

    upload PlayWeb
    Aplicativo web pelo navegador sem necessidade de instalação para ouvir audios no formato mp3.

---
+++ script.js
@@ -0,0 +1,18 @@
+// Função para formatar 1 em 01
+const zeroFill = n => {
+    return ('0' + n).slice(-2);
+}
+
+//Criar intervalo
+const interval = setInterval(() => {
+    //Captura o horário atual
+    const now = new Date();
+
+    //Formata a data conforme dd/mm/aaaa hh:mm.ss
+    const hora = zeroFill(now.getHours()) + ':' + zeroFill(now.getMinutes()) + ':' + zeroFill(now.getSeconds());
+    const data = zeroFill(now.getDate()) + '/' + zeroFill((now.getMonth() + 1)) + '/' + now.getFullYear() + '.';
+    
+    //Exibe na tela
+    document.getElementById('date').innerHTML = data
+    document.getElementById('hours').innerHTML = hora
+			}, 1000);
